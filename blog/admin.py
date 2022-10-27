79459ec0db5d98c30ac629c1ee76f1cf04b0715f
Khaue <khaueramos123@gmail.com>
2022-10-27 17:01:21 -0300

    site

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from operator import imod
+from django.contrib import admin
+
+from blog.models import Post
+from django.contrib import admin
+# Register your models here.
+
+
+admin.site.register(Post)
---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+
+# Create your models here.
+class Post(models.Model):
+    Titulo = models.CharField(max_length=255)
+    Resumo = RichTextField()
+    Conteudo = RichTextUploadingField()
+    Autor = models.ForeignKey(User, on_delete=models.PROTECT)
+    Data = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.Titulo
